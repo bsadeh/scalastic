beb4f184c23b04c6c0fa182e91d0470bc0143ec2
Benny Sadeh <benny.sadeh@gmail.com>
2012-06-22 11:39:24 -0700

    implementing the full api of SearchRequestBuilder in Searching

--- src/main/scala/com/traackr/scalastic/elasticsearch/Searching.scala
+++ src/main/scala/com/traackr/scalastic/elasticsearch/Searching.scala
@@ -3,9 +3,8 @@ package com.traackr.scalastic.elasticsearch
 import org.elasticsearch.action.search._
 import org.elasticsearch.index.query._, QueryBuilders._
 import org.elasticsearch.common.xcontent._
-import org.elasticsearch.search._, sort._, facet._, terms._
+import org.elasticsearch.search._, facet._, terms._, sort._, SortBuilders._
 import scala.collection._, JavaConversions._
-import scalaz._, Scalaz._
 
 trait Searching extends Query with Search with Multisearch with Percolate {
   self: Indexer =>
